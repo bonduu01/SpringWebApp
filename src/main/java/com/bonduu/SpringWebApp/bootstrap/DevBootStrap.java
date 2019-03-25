871b76bf380d2a19acd3b40d3d1aea880f1d6497
Peter.Molokwu <bonduu01@gmail.com>
2019-03-25 12:05:51 +0100

    DevBootStrap Assignment

--- src/main/java/com/bonduu/SpringWebApp/bootstrap/DevBootStrap.java
+++ src/main/java/com/bonduu/SpringWebApp/bootstrap/DevBootStrap.java
@@ -2,40 +2,63 @@ package com.bonduu.SpringWebApp.bootstrap;
 
 import com.bonduu.SpringWebApp.model.Author;
 import com.bonduu.SpringWebApp.model.Book;
+import com.bonduu.SpringWebApp.model.Publisher;
 import com.bonduu.SpringWebApp.repositories.AuthorRepository;
 import com.bonduu.SpringWebApp.repositories.BookRepository;
+import com.bonduu.SpringWebApp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
+
+    public DevBootStrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         initData();
     }
 
-    private AuthorRepository authorRepository;
-    private BookRepository bookRepository;
+    private void initData(){
 
-    public DevBootStrap(AuthorRepository authorRepository, BookRepository bookRepository) {
-        this.authorRepository = authorRepository;
-        this.bookRepository = bookRepository;
-    }
+        Publisher publisher = new Publisher();
+        publisher.setName("First Publishing House");
+        publisher.setAddress("12th Street, LA");
+        publisherRepository.save(publisher);
 
-    private void initData() {
-        Author mainAuthor = new Author("Peter", "Molokwu");
-        Book book1 = new Book("newDawn", "4546", "Eric Banner");
-        mainAuthor.getBooks().add(book1);
-        book1.getAuthors().add(mainAuthor);
-        authorRepository.save(mainAuthor);
+        //Eric
+        Author author1 = new Author("Eric", "Evans");
+        Book  book1 = new Book("Domain Driven Design", "1234", publisher);
+        author1.getBooks().add(book1);
+        book1.getAuthors().add(author1);
+
+        authorRepository.save(author1);
         bookRepository.save(book1);
 
-        Author secAuthor = new Author("Addy", "Snokkle");
-        Book book2 = new Book("Celebate", "8979", "Peter Shapiro");
-        secAuthor.getBooks().add(book2);
 
-        authorRepository.save(secAuthor);
+        //Rod
+        Author author2 = new Author("Rod", "Johnson");
+        Publisher publisher2 = new Publisher();
+
+        publisher2.setName("Second Publishing House");
+        publisher2.setAddress("11, Adu street, Ogba.");
+        publisherRepository.save(publisher2);
+
+        Book book2 = new Book("J2EE Development without EJB", "23444", publisher2 );
+        author2.getBooks().add(book2);
+        book2.getAuthors().add(author2);
+
+        authorRepository.save(author2);
         bookRepository.save(book2);
     }
 }
---
+++ src/main/java/com/bonduu/SpringWebApp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.bonduu.SpringWebApp.repositories;
+
+import com.bonduu.SpringWebApp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long > {
+}
